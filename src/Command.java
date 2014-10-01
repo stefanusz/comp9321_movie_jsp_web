2eb56adc58e395244adb2aa3dda135428fe30c08
Stefanus <z3442577@zmail.unsw.edu.au>
2014-10-01 16:12:55 +1000

    add of package

--- src/Command.java
+++
@@ -1,7 +0,0 @@
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-
-public interface Command {
-	public abstract boolean execute(HttpServletRequest request, HttpServletResponse response);
-}
