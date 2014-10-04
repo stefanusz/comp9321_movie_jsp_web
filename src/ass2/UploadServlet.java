0f6b830a4c6b5d127de6af121d185b74ae286c64
Stefanus Zhang <Stefanus@Stefanus-Zhangs-MacBook-Pro.local>
2014-10-04 17:33:58 +1000

    start on css application

---
+++ src/ass2/UploadServlet.java
@@ -0,0 +1,18 @@
+package ass2;
+
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet({ "/UploadServlet", "/upload" })
+@MultipartConfig
+public class UploadServlet extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	
+}
